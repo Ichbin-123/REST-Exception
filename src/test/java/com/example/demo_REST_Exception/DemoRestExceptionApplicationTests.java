2ef9aaadd2ddecb9ea9483846e3f5ddae2512b67
Marco Fuchs <furfaro.markus@gmail.com>
2025-10-08 18:30:44 +0200

    First Commit

---
+++ src/test/java/com/example/demo_REST_Exception/DemoRestExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_REST_Exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
